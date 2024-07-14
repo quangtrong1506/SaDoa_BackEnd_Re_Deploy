2a14de66e27aa7d08c7acedd7e75b12386e1e750
Quang Trong <quangtrong1506@gmail.com>
2024-07-14 19:50:27 +0700

    fix express-async-errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
 import './loadEnvironment.js';
 import router from './routes/index.js';
